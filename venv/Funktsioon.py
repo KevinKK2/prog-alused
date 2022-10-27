7ae9d6b8f20fc258c511368b4c7503eaa7ca18c5
Kevin <kevin.kiho@voco.ee>
2022-10-27 16:34:39 +0300

    Ülesanne: Funktsioon

---
+++ venv/Funktsioon.py
@@ -0,0 +1,36 @@
+# inside the function
+
+def func():
+    print("Im inside the function")
+func()
+
+# My name
+
+def my_name_is(name):
+    print(f"my_name_is {name}")
+my_name_is("Mari")
+
+def sum_six(num):
+    return (6 + num)
+
+print(sum_six(5))
+
+# Sum number
+
+def  sum_numbers(a, b):
+    return (a + b)
+
+print(sum_numbers(10, 25))
+
+# USD to Eur
+
+def usd_to_eur(dollar):
+    return round(dollar * 0.8)
+print(usd_to_eur(100))
+
+
+
+
+
+
+
